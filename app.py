1143044e63fc73ea140d37e372b26685025373bb
Keila Andrea Flores Garcete <keilaflores889@gmail.com>
2024-08-14 09:49:30 -0400

    modificaciones

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,17 +6,21 @@ app = Flask(__name__)
 def inicio():
     return "hola mundo desde el backend"
 
-
-#endpoint o ruta
+# endpoint o ruta
 @app.route('/contacto')
 def contacto():
-    return "<h3>Introduccion HTML desde el servidor</h3>"
+    return "<h3>Introduciendo HTML desde el servidor</h3>"
 
 @app.route('/contacto2')
 def contacto2():
     return render_template('contacto.html')
 
-#aqui donde se arranca el servidor
+@app.route('/guardar-mascota', methods=['POST'])
+def guardarMascota():
+    print(request.form)
+    nombreMascota = request.form.get('txtNombreMascota')
+    return f"Ya llego tu mascota <strong>{nombreMascota}</strong> al servidor"
+
 # se pregunta por el proceso principal
 if __name__=='__main__':
     app.run(debug=True)
