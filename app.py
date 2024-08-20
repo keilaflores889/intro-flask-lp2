ce046ae77600d1435723f5cf808c9221197f1307
Keila Andrea Flores Garcete <keilaflores889@gmail.com>
2024-08-20 10:29:03 -0400

    dao para ciudad

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for,flash
+from dao.CiudadDao import CiudadDao
 
 app = Flask(__name__)
+
+# flash requiere esta sentencia
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/inicio')
 def inicio():
@@ -14,6 +18,29 @@ def contacto():
 @app.route('/contacto2')
 def contacto2():
     return render_template('contacto.html')
+
+@app.route('/ciudades')
+def ciudades():
+    return render_template('ciudades.html')
+
+@app.route('/guardar-ciudad', methods=['POST'])
+def guardarCiudad():
+    ciudad = request.form.get('txtDescripcion').strip()
+    if ciudad == None or len(ciudad) < 1:
+       # mostrar un mensaje al usuario
+       flash('Debe escribir algo en la descripcion', 'warning')
+    
+       # redireccionar a la vista ciudades
+       return redirect(url_for('ciudades'))
+    
+    ciudaddao = CiudadDao()
+    ciudaddao.guardarCiudad(ciudad.upper())
+
+    # mostrar un mensaje al usuario
+    flash('Guardado exitoso', 'success')
+
+    # redireccionar a la vista ciudades 
+    return redirect(url_for('ciudades'))
 
 @app.route('/guardar-mascota', methods=['POST'])
 def guardarMascota():
--- dao/CiudadDao.py
+++ dao/CiudadDao.py
@@ -12,9 +12,45 @@ class CiudadDao:
         #objeto conexion
         conexion = Conexion()
         con = conexion.getConexion()
+        cur = con.cursor()
         try:
-            pass
-        except:
-            pass
+          cur.execute(ciudadSQL)
+          #trae datos de db
+          lista_ciudades = cur.fetchall()
+          #retorno de datos
+          return lista_ciudades
+        except con.Error as e:
+            print(e)
         finally:
-            pass
+            cur.close()
+            con.close()
+
+    def guardarCiudad(self, descripcion):
+        
+        insertCiudadSQL = """
+        INSERT INTO ciudades(descripcion) VALUES(%s)
+        """
+
+        conexion = Conexion()
+        con = conexion.getConexion()
+        cur = con.cursor()
+
+        #Ejecucion exitosa
+        try:
+            cur.execute(insertCiudadSQL, (descripcion,))
+            #se confirma la isercion
+            con.commit()
+
+            return True
+
+        #si algo falla aqui
+        except con.Error as e:
+            print(e)
+            
+        #siempre se va a ejecutar
+        finally:
+            cur.close()
+            con.close()
+
+        return False    
+          
